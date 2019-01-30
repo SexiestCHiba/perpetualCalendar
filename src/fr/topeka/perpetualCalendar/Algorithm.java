bb83b9c50a7693c8a90169f017b7d82fa76f6979
Quentin Legot <quentin3157.github@gmail.com>
2019-01-30 15:27:19 +0100

    Initial commit

---
+++ src/fr/topeka/perpetualCalendar/Algorithm.java
@@ -0,0 +1,311 @@
+package fr.topeka.perpetualCalendar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Quentin
+ *
+ */
+public class Algorithm {
+
+	private int baseYear, baseMonth, baseDay, baseWeekDay, year, month, day, result;
+	private String weekDay = "";
+	private List<String> weekDayList= new ArrayList<>();
+	
+	public Algorithm() {
+		Scanner sc = new Scanner(System.in);
+		weekDayList.add("Lundi");
+		weekDayList.add("Mardi");
+		weekDayList.add("Mercredi");
+		weekDayList.add("Jeudi");
+		weekDayList.add("Vendredi");
+		weekDayList.add("Samedi");
+		weekDayList.add("Dimanche");
+		System.out.println("Saisissez l'année de base");
+		baseYear = sc.nextInt();
+		System.out.println("Saisissez le mois de base");
+		baseMonth = sc.nextInt();
+		System.out.println("Saisissez le jour de base");
+		baseDay = sc.nextInt();
+		setWeekDay(sc);
+		baseWeekDay = weekDayList.indexOf(weekDay);
+		System.out.println(baseWeekDay);
+		System.out.println("Saisissez l'année de la date que vous chercher");
+		year = sc.nextInt();
+		System.out.println("Saisissez le mois de la date que vous chercher");
+		month = sc.nextInt();
+		System.out.println("Saisissez le jour de la date que vous chercher");
+		day = sc.nextInt();
+		if(ifDateExist()) {
+			System.out.println("La date existe");
+			int numbersOfDayOfDifference;
+			if(ifBaseDateisBeforeDate()) {
+				System.out.println("La date de base est avant la date que vous chercher");
+				numbersOfDayOfDifference = numbersOfDateOfDifferenceAfter();
+				System.out.println(numbersOfDayOfDifference);
+				result = (numbersOfDayOfDifference+baseWeekDay)%7;
+				System.out.println(weekDayList.get(result));
+			}else {
+				System.out.println("La date de base est après la date que vous chercher");
+				numbersOfDayOfDifference = numbersOfDateOfDifferenceBefore();
+				System.out.println(numbersOfDayOfDifference);
+				result = (numbersOfDayOfDifference+baseWeekDay)%7;
+				if(result<0) result+=7;
+				System.out.println(weekDayList.get(result));
+			}
+		}else {
+			System.out.println("La date n'existe pas");
+		}
+	}
+	
+	private boolean ifDateExist() {
+		if(baseMonth<1 || baseDay<1 || month<1 || day<1 || baseMonth>12 || month>12 ||baseDay>31 || day>31 || baseYear<1584 || year<1584) {
+			System.out.println("1: La date renseignée n'existe pas");
+			return false;
+		}else {
+			if(baseDay==31) {
+				if(!(baseMonth==1 || baseMonth==3 || baseMonth==5 || baseMonth==7 || baseMonth==8 || baseMonth==10 || baseMonth==12)) {
+					System.out.println("2: La date renseignée n'existe pas");
+					return false;
+				}
+			}
+			if(day==31) {
+				if(!(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)) {
+					System.out.println("3: La date renseignée n'existe pas");
+					return false;
+				}
+			}
+			if(baseDay==29 && baseMonth==2) {
+				if(!(baseYear%400==0)) {
+					if(!(baseYear%4==0 && !(baseYear%100==0))) {
+						System.out.println("4: La date renseignée n'existe pas");
+						return false;
+					}
+				}
+			}
+			if(day==29 && month==2) {
+				if(!(year%400==0)) {
+					if(!(year%4==0 && !(year%100==0))) {
+						System.out.println("5: La date renseignée n'existe pas");
+						return false;
+					}
+				}
+			}
+			if((baseYear==1584 && baseMonth<11) || (year==1584 && month<11)) {
+				System.out.println("6: La date renseignée n'existe pas");
+				return false;
+			}
+			return true;
+		}
+	}
+	
+	private int numbersOfDateOfDifferenceAfter() {
+		int var=0;
+		boolean finish=false;
+		int[] list = new int[3];
+		int year=this.baseYear, month=this.baseMonth, day=this.baseDay;
+		while(finish==false) {
+			if(year>this.year) {
+				finish=true;
+			}else {
+				if(year==this.year) {
+					if(month>this.month) {
+					finish=true;
+					}else {
+						if(month==this.month) {
+							if(day>=this.day) {
+								finish=true;
+							}else {
+								list = addADay(day, month, year);
+								day=list[0];
+								month=list[1];
+								year=list[2];
+								var++;
+							}
+						}else {
+							list = addADay(day, month, year);
+							day=list[0];
+							month=list[1];
+							year=list[2];
+							var++;
+						}
+					}
+				}else {
+					list = addADay(day, month, year);
+					day=list[0];
+					month=list[1];
+					year=list[2];
+					var++;
+				}
+			}
+		}
+		
+		return var;
+	}
+	
+	
+	
+	private int[] addADay(int day,int month,int year){
+		int[] liste = new int[3];
+		if(day==30 || day==31) {
+			if(day==31) {
+				day=1;
+				if(month==12) {
+					year++;
+					month=1;
+				}else {
+					month++;
+				}
+			}else {
+				if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12) {
+					day=31;
+				}else {
+					day=1;
+					month++;
+				}
+			}
+		}else {
+			if(month==2) {
+				if(day==28) {
+					if(year%400==0) {
+						day++;
+					}else {
+						if(year%4==0 && !(year%100==0)) {
+							day++;
+						}else {
+							day=1;
+							month=3;
+						}
+					}
+				}else {
+					if(day==29) {
+						day=1;
+						month=3;
+					}else {
+						day++;
+					}
+				}
+			}else {
+				day++;
+			}
+		}
+		liste[0] = day;
+		liste[1] = month;
+		liste[2] = year;
+		return liste;
+		
+	}
+	
+	private int numbersOfDateOfDifferenceBefore() {
+		int var=0;
+		boolean finish=false;
+		int[] list = new int[3];
+		int year=this.baseYear, month=this.baseMonth, day=this.baseDay;
+		while(finish==false) {
+			if(year<this.year) {
+				finish=true;
+			}else {
+				if(year==this.year) {
+					if(month<this.month) {
+					finish=true;
+					}else {
+						if(month==this.month) {
+							if(day<=this.day) {
+								finish=true;
+							}else {
+								list = removeADay(day, month, year);
+								day=list[0];
+								month=list[1];
+								year=list[2];
+								var--;
+							}
+						}else {
+							list = removeADay(day, month, year);
+							day=list[0];
+							month=list[1];
+							year=list[2];
+							var--;
+						}
+					}
+				}else {
+					list = removeADay(day, month, year);
+					day=list[0];
+					month=list[1];
+					year=list[2];
+					var--;
+				}
+			}
+		}
+		return var;
+	}
+
+	
+	private int[] removeADay(int day,int month,int year){
+		int[] liste = new int[3];
+		if(day==1) {
+			if(month==1) {
+				year--;
+				month=12;
+				day=31;
+			}else {
+				if(month-1==2) {
+					if(year%400==0) {
+						day=29;
+						month=2;
+					}else {
+						if(year%4==0 && !(year%100==0)) {
+							day=29;
+							month=2;
+						}else {
+							day=28;
+							month=2;
+						}
+					}
+				}else {
+					if(month-1==1 || month-1==3 || month-1==5 || month-1==7 || month-1==8 || month-1==10) {
+						day=31;
+						month--;
+					}else {
+						day=30;
+						month--;
+					}
+				}	
+			}
+			
+			
+		}else {
+			day--;
+		}
+		liste[0] = day;
+		liste[1] = month;
+		liste[2] = year;
+		return liste;
+		
+	}
+	
+	private boolean ifBaseDateisBeforeDate() {
+		if(baseYear<year) return true;
+		else if(baseYear==year) {
+			if(baseMonth<month) return true;
+			else if(baseMonth==month) {
+				if(baseDay<day) return true;
+				else return false;
+			}else return false;
+		}else return false;
+	}
+	
+	private void setWeekDay(Scanner sc) {
+		boolean fail=false;
+		while(!weekDayList.contains(weekDay)) {
+		if(fail) System.out.println("Erreur: Veuillez écrire correctement le jour de la semaine");
+		
+		System.out.println("Saisissez le jour de la semaine de cette date");
+		weekDay = sc.nextLine();
+		fail=true;
+		}
+	}
+	
+}
---
+++ src/fr/topeka/perpetualCalendar/PerpetualCalendar.java
@@ -0,0 +1,16 @@
+package fr.topeka.perpetualCalendar;
+
+/**
+ * 
+ * @author Quentin Legot
+ *
+ */
+public class PerpetualCalendar {
+	
+	public static void main(String[] args) {
+	@SuppressWarnings("unused")
+	Algorithm algorithm = new Algorithm();
+	}
+	
+}
+	
